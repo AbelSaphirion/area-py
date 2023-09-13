850d4869ab8997a9a63a29117109fd298a16a5a5
Strelchenko Aleksandr <telikinez@gmail.com>
2023-09-13 16:38:23 +0300

    Complete assigment

--- src/areapy/areapy.py
+++ src/areapy/areapy.py
@@ -1,1 +1,84 @@
-pass
+import abc
+import math
+
+class Figure(abc.ABC):
+    
+    def __init__(self) -> None:
+        pass
+    
+    @property
+    @abc.abstractmethod
+    def area() -> float:
+        """Area of a figure."""
+        pass
+
+class Circle(Figure):
+    __slots__ = ['r']
+    
+    def __init__(self, r : float) -> None:
+        super().__init__()
+        if(r is None or math.isnan(r)):
+            raise ValueError("Radius must be a valid number")
+        if(r <= 0):
+            raise ValueError("Radius must be a valid number greater then 0.")
+        self.r = r
+    
+    @property
+    def area(self) -> float:
+        """Area of a circle."""
+        return math.pi * self.r * self.r
+
+class Triangle(Figure):
+    __slots__ = ['a', 'b', 'c']
+    
+    def __init__(self, a : float, b : float, c : float) -> None:
+        super().__init__()
+        if (a is None or b is None or c is None or
+            math.isnan(a) or math.isnan(b) or math.isnan(c)):
+            raise ValueError("Side size must be a valid number")
+        if (a <= 0 or b <= 0 or c <= 0):
+            raise ValueError("Side size must be a valid number greater then 0.")
+        if (a + b <= c or
+            a + c <= b or
+            b + c <= a):
+            raise ValueError("Triangle is not possible.")
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    @property
+    def area(self) -> float:
+        """Area of a triangle."""
+        lenghts = (self.a, self.b, self.c)
+        #Check if triangle is right
+        maxidx = lenghts.index(max(lenghts))
+        previdx = maxidx - 1
+        nextidx = 0 if maxidx + 1 >= 3 else maxidx + 1
+        if math.isclose(lenghts[previdx] ** 2 + lenghts[nextidx] ** 2, lenghts[maxidx] ** 2):
+            #If triangle is right
+            return lenghts[previdx] * lenghts[nextidx] / 2
+        else:
+            #If triangle is not right
+            s = sum(lenghts) / 2
+            return math.sqrt(s * (s - lenghts[0]) * (s - lenghts[1]) * (s - lenghts[2]))
+        
+
+def getarea(figure: str, *args: float) -> float:
+    """Get an area of a given figure.
+
+    Keyword arguments:
+    figure -- figure type ("circle", "triangle")
+    args -- figure sizes (radius for circle; a, b, c sizes of a triangle)
+    """
+    if figure.lower() == "circle":
+        if len(args) != 1:
+            raise ValueError("Number of additional arguments must be 1 in order to find a circle area.")
+        fig = Circle(*args)
+        return fig.area
+    elif figure.lower() == "triangle":
+        if len(args) != 3:
+            raise ValueError("Number of additional arguments must be 3 in order to find a triangle area.")
+        fig = Triangle(*args)
+        return fig.area
+    else:
+        raise ValueError("Unimplemented figure.")
--- tests/test_areapy.py
+++ tests/test_areapy.py
@@ -1,3 +1,137 @@
 import pytest
+import areapy
+import math
 
-pass
+class TestCircle():
+    
+    def test_r_negative(self):
+        with pytest.raises(ValueError):
+            areapy.Circle(-3)
+        with pytest.raises(ValueError):
+            areapy.Circle(-5.11)
+        with pytest.raises(ValueError):
+            areapy.Circle(-661.13)
+            
+    def test_r_None_or_nan(self):
+        with pytest.raises(ValueError):
+            areapy.Circle(None)
+        with pytest.raises(ValueError):
+            areapy.Circle(float("nan"))
+    
+    def test_r_zero(self):
+        with pytest.raises(ValueError):
+            areapy.Circle(0)
+            
+    def test_r_positive(self):
+        areapy.Circle(3)
+        areapy.Circle(5.23)
+        areapy.Circle(777.77)
+        
+    def test_area(self):
+        c1 = areapy.Circle(2)
+        c2 = areapy.Circle(4.23)
+        c3 = areapy.Circle(32.77)
+        assert math.isclose(math.pi * 2 ** 2, c1.area)
+        assert math.isclose(math.pi * 4.23 ** 2, c2.area)
+        assert math.isclose(math.pi * 32.77 ** 2, c3.area)
+
+class TestTriangle():
+    
+    def test_abc_negative(self):
+        with pytest.raises(ValueError):
+            areapy.Triangle(-3, 5, 1)
+        with pytest.raises(ValueError):
+            areapy.Triangle(5.11, -1.3, 8)
+        with pytest.raises(ValueError):
+            areapy.Triangle(-33, -2.1, 2)
+        with pytest.raises(ValueError):
+            areapy.Triangle(33, 2.13, -11)
+            
+    def test_abc_None_or_nan(self):
+        with pytest.raises(ValueError):
+            areapy.Triangle(None, 1, 2)
+        with pytest.raises(ValueError):
+            areapy.Triangle(1, 2, float("nan"))
+        with pytest.raises(ValueError):
+            areapy.Triangle(None, 2, float("nan"))
+    
+    def test_abc_zero(self):
+        with pytest.raises(ValueError):
+            areapy.Triangle(0, 1, 2)
+        with pytest.raises(ValueError):
+            areapy.Triangle(1, 0, 2)
+        with pytest.raises(ValueError):
+            areapy.Triangle(0, 0, 2)
+        with pytest.raises(ValueError):
+            areapy.Triangle(2, 1, 0)
+            
+    def test_abc_positive_valid(self):
+        areapy.Triangle(3, 5, 7)
+        areapy.Triangle(5.23, 4.3, 3)
+        areapy.Triangle(1, 1, 1)
+    
+    def test_abc_positive_nonvalid(self):
+        with pytest.raises(ValueError):
+            areapy.Triangle(1, 10, 2)
+        with pytest.raises(ValueError):
+            areapy.Triangle(77, 2.311, 1)
+        with pytest.raises(ValueError):
+            areapy.Triangle(13, 12, 100)
+        
+    def test_area(self):
+        t1 = areapy.Triangle(7, 24, 25)
+        t2 = areapy.Triangle(3.23, 2, 2)
+        t3 = areapy.Triangle(5, 7, 10)
+        t4 = areapy.Triangle(25, 24, 7)
+        assert math.isclose(7 * 24 / 2, t1.area)
+        s = (3.23 + 2 + 2) / 2
+        assert math.isclose(math.sqrt(s * (s - 3.23) * (s - 2) * (s - 2)), t2.area)
+        s = (5 + 7 + 10) / 2
+        assert math.isclose(math.sqrt(s * (s - 5) * (s - 7) * (s - 10)), t3.area)
+        assert math.isclose(7 * 24 / 2, t4.area)
+        
+class TestGetarea():
+    circletxt = "circle"
+    triangletxt = "triangle"
+    
+    def test_wrong_figure(self):
+        with pytest.raises(ValueError):
+            areapy.getarea("Pentagram", 3, 2, 2, 3, 2)
+        with pytest.raises(ValueError):
+            areapy.getarea("norway", 100)
+    
+    def test_circle_wrong_args(self):
+        with pytest.raises(ValueError):
+            areapy.getarea(self.circletxt)
+        with pytest.raises(ValueError):
+            areapy.getarea(self.circletxt, 3, 3, 3)
+    
+    def test_circle_area(self):
+        c1 = areapy.getarea(self.circletxt, 3)
+        c2 = areapy.getarea(self.circletxt, 5.23)
+        c3 = areapy.getarea(self.circletxt, 33.77)
+        assert math.isclose(math.pi * 3 ** 2, c1)
+        assert math.isclose(math.pi * 5.23 ** 2, c2)
+        assert math.isclose(math.pi * 33.77 ** 2, c3)
+    
+    def test_triangle_wrong_args(self):
+        with pytest.raises(ValueError):
+            areapy.getarea(self.triangletxt)
+        with pytest.raises(ValueError):
+            areapy.getarea(self.triangletxt, 5)
+    
+    def test_triangle_area(self):
+        t1 = areapy.getarea(self.triangletxt, 7, 24, 25)
+        t2 = areapy.getarea(self.triangletxt, 3.33, 2, 2)
+        t3 = areapy.getarea(self.triangletxt, 5, 8, 10)
+        t4 = areapy.getarea(self.triangletxt, 25, 24, 7)
+        assert math.isclose(7 * 24 / 2, t1)
+        s = (3.33 + 2 + 2) / 2
+        assert math.isclose(math.sqrt(s * (s - 3.33) * (s - 2) * (s - 2)), t2)
+        s = (5 + 8 + 10) / 2
+        assert math.isclose(math.sqrt(s * (s - 5) * (s - 8) * (s - 10)), t3)
+        assert math.isclose(7 * 24 / 2, t4)
+        
+    def test_upper_case(self):
+        areapy.getarea("Circle", 2)
+        areapy.getarea("Triangle", 5, 5, 5)
